1e3a678bf7f43066ca5e4153f04e886220164722
Oliver West <oliversimon.west@gmail.com>
2023-10-02 16:12:53 +0100

    set up browser router with empty pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider, createTheme } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
 const theme = createTheme({
     palette: {
@@ -32,7 +33,9 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ThemeProvider>
     </React.StrictMode>
 );
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+function MainPage() {
+    return (
+        <div>
+            <p>Main Page</p>
+        </div>
+    );
+}
+
+export default MainPage;
